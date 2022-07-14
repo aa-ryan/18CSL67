f32b59ea0ed47658fd2ce0ac815b6f59a729a176
aa-ryan <aryansinha6363@gmail.com>
2022-07-15 03:46:12 +0530

    pg6 test added

---
+++ PG6/test.cpp
@@ -0,0 +1,92 @@
+#define GL_SILENCE_DEPRECATION
+#include<glut/GLUT.h>
+
+GLfloat T = 0;
+
+void Spin() {
+	T = T + 0.5;
+	if (T > 360)
+		T = 0;
+	glutPostRedisplay();
+}
+
+void Draw() {
+	GLfloat Pos[] = {0, 1, 0, 1};
+	GLfloat Col[] = {1, 0, 0, 1};
+
+	GLfloat M1[] = {0, 0, 1, 1};
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	glLightfv(GL_LIGHT0, GL_POSITION, Pos);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, Col);
+
+	gluLookAt(0, 1, 3, 0, 0, 0, 0, 1, 0);
+	glRotatef(T, 0, 1, 0);
+
+	glPushMatrix();
+		glScalef(1, 0.05, 1);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(-0.5, -0.5, -0.5);
+		glScalef(0.05, 1, 0.05);
+		glutSolidCube(1);
+	glPopMatrix();
+
+
+	glPushMatrix();
+		glTranslatef(0.5, -0.5, -0.5);
+		glScalef(0.05, 1, 0.05);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(0.5, -0.5, 0.5);
+		glScalef(0.05, 1, 0.05);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glPushMatrix();
+		glTranslatef(-0.5, -0.5, 0.5);
+		glScalef(0.05, 1, 0.05);
+		glutSolidCube(1);
+	glPopMatrix();
+
+	glPushAttrib(GL_ALL_ATTRIB_BITS);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, M1);
+		glPushMatrix();
+			glTranslatef(0, 0.25, 0);
+			glutSolidTeapot(0.25);
+		glPopMatrix();
+	glPopAttrib();
+
+	glutSwapBuffers();
+}
+
+void MyInit() {
+	glEnable(GL_DEPTH_TEST);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-1, 1, -1, 1, 2, 10);
+
+	glMatrixMode(GL_MODELVIEW);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+}
+
+int main(int argc, char *argv[])
+{
+	glutInit(&argc, argv);
+	glutInitWindowSize(600, 600);
+	glutInitWindowPosition(100, 100);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutCreateWindow("Table and Teapot");
+	MyInit();
+	glutDisplayFunc(Draw);
+	glutIdleFunc(Spin);
+	glutMainLoop();
+	return 0;
+}
